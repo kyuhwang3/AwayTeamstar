6ff170220ce7409a2086720f8638629170dd973e
Kyu Hwang Kim <kyuhwang.kim@lge.com>
2018-06-22 15:15:57 +0900

    project M

---
+++ projectM/app.js
@@ -0,0 +1,32 @@
+// app.js
+
+// [LOAD PACKAGES]
+var express     = require('express');
+var app         = express();
+var bodyParser  = require('body-parser');
+var mongoose    = require('mongoose');
+
+// [CONFIGURE APP TO USE bodyParser]
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// [CONFIGURE SERVER PORT]
+var port = process.env.PORT || 8080;
+
+// [CONFIGURE ROUTER]
+var Client = require('./models/client');
+var router = require('./router')(app, Client);
+
+var db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', function(){
+    // CONNECTED TO MONGODB SERVER
+    console.log("Connected to mongod server");
+});
+
+mongoose.connect('mongodb+srv://kkim0129:Enfl89qjs!@cluster0-3yigh.mongodb.net/test?retryWrites=true');
+
+// [RUN SERVER]
+var server = app.listen(port, function(){
+    console.log("Express server has started on port " + port)
+});
---
+++ projectM/router/index.js
@@ -0,0 +1,30 @@
+// routes/index.js
+
+module.exports = function (app, Client) {
+    app.get('/', function (req, res){
+        res.end("Project M");
+    });
+    app.get('/api/clients', function (req, res) {
+        Client.find(function(err, books){
+            if(err) return res.status(500).send({error: 'database failure'});
+            res.json(books);
+        })
+    });
+
+    app.post('/api/clients', function (req, res) {
+        var client = new Client();
+        client.name = req.body.name;
+        //console.log(JSON.parse(req.body.name));
+        console.log(req.body);
+        client.save(function(err){
+            if(err){
+                console.error(err);
+                res.json({result: 0});
+                return;
+            }
+    
+            res.json({result: 1});
+    
+        });
+    });
+}

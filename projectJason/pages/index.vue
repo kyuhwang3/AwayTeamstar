074f5d877c5a92e5d4f041443aaad64b83275891
Kyu Hwang Kim <kyuhwang.kim@lge.com>
2018-11-13 20:10:16 -0600

    add projectJason

---
+++ projectJason/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <section class="section is-large">
+    <div class="container has-text-centered">
+      <h1 class="title">Hi, I'm Kyu Hwang Kim</h1>
+      <h2 class="subtitle">a Software Developer based in Toronto</h2>
+    </div>
+  </section>
+</template>
